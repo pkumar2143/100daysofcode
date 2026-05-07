3557a6b71571ac74d3bb3386e960471aad913fe6
northmindstudies <northmindstudies@gmail.com>
2026-05-07 14:23:52 -0400

    Player's turn complete

--- 100doc/day011_blackjack.py
+++ 100doc/day011_blackjack.py
@@ -13,7 +13,7 @@
 import random
 
 def draw():
-    deck = [1,2,3,4,5,6,7,8,9,10,10,10,10] # A = 1 or 11 ; J = Q = K = 10; Initialized on drawing. Later we'll create a fixed deck
+    deck = [11,2,3,4,5,6,7,8,9,10,10,10,10] # A = 11 to start, if sum > 21, then A becomes 1. ; J = Q = K = 10; Initialized on drawing. Later we'll create a fixed deck
     return random.choice(deck)
 
 def ace_value_replace(hand):
@@ -41,23 +41,6 @@ def check_21(hand):
         return "Short"
 
 
-# If at ANY time a "1" (Ace) is dealt, then you have to compare two hand lists
-# One original
-# One replacing the first instance of 1 with 11.
-
-def ace_replace(hand):
-    '''
-    Assume there is a 1 in the hand.
-    Note: we can have AT MOST one 11 in hand, because if we have more, it's BUST! 
-    '''
-    replaced_hand = hand.copy() # created a shallow copy to not affect original hand.
-    replaced_hand[replaced_hand.index(1)] = 11 # replace the first 1 with 11
-
-    return replaced_hand
-
-    
-
-
 print(30*'\n' + '++++++++++ Welcome to Blackjack +++++++++++')
 
 dealer_hand = []
@@ -68,54 +51,45 @@ for i in range(2):
     player_hand.append(draw())
     dealer_hand.append(draw())
 
-######### DEBUGGING ###########
-player_hand = [1,1]
-###############################
-
 print(f"Player's Hand = {player_hand}, total = {sum(player_hand)}")
-
-if 1 in player_hand:
-    alt_hand_sum = sum(player_hand) + 10
-    print(f"Alternative Total = {alt_hand_sum}")
-    
-
 print(f"Dealer's Hand = [{dealer_hand[0]}, ?]") # Only display one of dealer's cards.
 
 # Player's Action
 player_stand_or_draw = 0 # Initialized for 1st iter.
-while (player_stand_or_draw != 's') and (check_21(player_hand) != 'Bust'): # So long as player DOES NOT stand and their hand is NOT a BUST
-    if check_21(player_hand) == 'Hold': #If it equals 21, then automatically Hold, and break out of the loop
-        print("Player has 21 ... Move to Dealer")
+while (player_stand_or_draw != 's') and (check_21(player_hand) != 'Bust'):           # So long as player DOES NOT stand and their hand is NOT a BUST
+    if check_21(player_hand) == 'Hold':                                              # If it equals 21, then automatically Hold, and break out of the loop
+        print("\nPlayer has 21 ... Move to Dealer\n")
         break
 
     player_stand_or_draw = 0 # reset stand-draw choice after each iter.
     # Make sure Player chooses ONLY 's' or 'd'.
-    while not ( (player_stand_or_draw == 's') or (player_stand_or_draw == 'd') ): 
-        player_stand_or_draw = input(">>> Player: Stand (s) or Draw (d): ")            # Keep asking them.
+    while not ( (player_stand_or_draw == 's') or (player_stand_or_draw == 'd') ):    
+        player_stand_or_draw = input("\n>>> Player: Stand (s) or Draw (d): ")           # Keep asking them.
 
     # After determining 's' or 'd' ...
     if player_stand_or_draw == 'd': # draw card
-        print('>>> Player Draws!')
+        print('\n>>> Player Draws!\n')
         player_hand.append(draw())
-        print(f"Player's Hand = {player_hand}, total = {sum(player_hand)}")        # Print out new hand & total
 
-        if 1 in player_hand:
-            alt_hand_sum = sum(player_hand) + 10
-            print(f"Alternative Total = {alt_hand_sum}")
+        # This will replace the Ace value of 11 each time the player busts.
+        if ( 11 in player_hand ) and ( sum(player_hand) > 21 ): # If there is an 11 in Player's hand and if the sum (after drawing a new card) > 1
+            player_hand[player_hand.index(11)] = 1 # replace the value.
+            print("Player's one Ace Value of 11 is replaced by 1...")
+        
+        print(f"\nPlayer's Hand = {player_hand}, Total = {sum(player_hand)}, Status = {check_21(player_hand)}\n")        # Print out new hand & total
 
-        print(check_21(player_hand))
         continue
+    
     elif player_stand_or_draw == 's':                                              # If player stands,
-        print('>>> Player Stands!')
-        print(f">> >> >> >> >> Final Player's Hand = {player_hand}, Final Player Total = {sum(player_hand)} << << << << <<")
-
-        exist_1_BOOL, replaced_hand = ace_sum_checker(player_hand)
-        if exist_1_BOOL:
-            print(f"Player's Final Hand (REPLACING '1') = {replaced_hand}, Final Player Total (REPLACING '1') = {sum(replaced_hand)}")
-            print(check_21(replaced_hand))
+        print('\n>>> Player Stands!\n')
+        print(f"\n>> >> >> >> >> Final Player's Hand = {player_hand}, Final Player Total = {sum(player_hand)} << << << << <<\n")
 
         break                                                                      # Break out of loop
 
+
+# Dealer's Action
+#print("\n>>> DEALER'S TURN\n")
+#print(f"\n>>> DEALER'S Hand:{dealer_hand}, DEALER'S Total:{sum(dealer_hand)}\n")
 
 
 #if __name__ == "__main__":
